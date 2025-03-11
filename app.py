e723a6dd3b2daa528dc7406733f624786e4b2f12
SOUNDARYA456477 <cs0806410@gmail.com>
2025-03-11 12:14:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,29 +1,27 @@
 from flask import Flask, jsonify
 
-app = Flask('__name__')
+app = Flask(__name__)  
 
 
-@app.route('/students-list')
-def students_list():
-    students=[{
-        'student_name': 'std1',
-        'age':21,
-        "email":'sound@gmail.com'
-   },
-        {'student_name': 'std2',
-        'age':22,
-        "email":'sound@gmail.com'
-   },
-        {'student_name': 'std3',
-        'age':23,
-        "email":'sound@gmail.com'
-   }
+students = [
+    {'id': 1, 'name': 'dev', 'age': '19', 'dept': 'kssem'},
+    {'id': 2, 'name': 'dev', 'age': '19', 'dept': 'kssem'},
+    {'id': 3, 'name': 'dev', 'age': '19', 'dept': 'kssem'}
 ]
+
+@app.route('/students-list', methods=['GET'])
+def students_list():
     return jsonify(students)
 
+@app.route('/students-list/<int:student_id>', methods=['GET'])  
+def get_student_by_id(student_id):
+   
+    student = next((s for s in students if s['id'] == student_id), None)
+
+    if student:
+        return jsonify(student)
+    else:
+        return jsonify({'error': 'Student not found'}), 404  
+
 if __name__ == '__main__':
-    app.run(
-        host='127.0.0.1',
-        port=5010,
-        debug=True
-    )
+    app.run(host='0.0.0.0', port=5005, debug=True)
